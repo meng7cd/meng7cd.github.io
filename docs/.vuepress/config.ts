62d04be71cb65db0770a94ae78823ef3e3f98260
Meng <meng.cheng@d1m.cn>
2023-07-28 14:32:28 +0800

    feat(init): 初始化项目

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,47 @@
+import { defineUserConfig, defaultTheme } from "vuepress";
+
+// https://v2.vuepress.vuejs.org/zh/reference/config.html
+
+export default defineUserConfig({
+  // 语言
+  lang: "zh-CN",
+  // 标题
+  title: "你好，Meng ！",
+  // 描述
+  description: "这是我的第一个站点",
+  // head标签集
+  head: [["link", { rel: "icon", href: "/images/logo.png" }]],
+  // 多语言设置
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "Meng",
+      description: "My docs !",
+    },
+  },
+  theme: defaultTheme({
+    // logo
+    logo: "/images/logo.png",
+    // 默认主题配置
+    navbar: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "文档",
+        link: "/docs/",
+      },
+    ],
+    // 多语言
+    locales: {
+      "/": {
+        selectLanguageName: "简体中文",
+      },
+    },
+  }),
+  // 开发配置 -> 服务器端口
+  port: 8080,
+  // 开发配置 -> 是否在开发服务器启动后打开浏览器
+  open: false,
+});
